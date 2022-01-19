c6b594f633022f30ff2e79d9745f1301dd25a29e
WePlayFleX <alex.kamai@ya.ru>
2022-01-20 00:23:55 +0300

    V1

--- ENGLOBJ/ENGLOBJ.cpp
+++ ENGLOBJ/ENGLOBJ.cpp
@@ -5,10 +5,20 @@ using namespace std;
 class Distance
 {
 public:
-	void setdist(int ft, float in)
+	/*void setdist(int ft, float in)
 	{
 		feet = ft;
 		inches = in;
+	}*/
+
+	Distance() : feet(0), inches(0.0)
+	{
+
+	}
+
+	Distance(int ft, float in) : feet(ft), inches(in)
+	{
+
 	}
 
 	void getdist()
@@ -24,6 +34,8 @@ public:
 		cout << feet << "\'-" << inches << '\"';
 	}
 
+	void add_dist(Distance, Distance);
+
 private:
 	int feet;
 	float inches;
@@ -33,16 +45,37 @@ private:
 
 int main()
 {
-	Distance dist1, dist2;
-	dist1.setdist(11, 6.25);
-	dist2.getdist();
+	Distance dist1, dist3;
+	Distance dist2(11, 6.25);
+
+	/*dist1.setdist(11, 6.25);*/
+	/*dist2.getdist();*/
+
+	dist1.getdist();
+	dist3.add_dist(dist1, dist2);
 
 	cout << "\ndist1 = ";
 	dist1.showdist();
 	cout << "\ndist2 = ";
 	dist2.showdist();
+	cout << "\ndist3 = ";
+	dist3.showdist();
 	cout << endl;
 
 	system("pause");
 	return 0;
+}
+
+void Distance::add_dist(Distance d2, Distance d3)
+{
+	inches = d2.inches + d3.inches;
+	feet = 0;
+
+	if (inches >= 12.0)
+	{
+		inches -= 12.0;
+		feet++;
+	}
+
+	feet += d2.feet + d3.feet;
 }
